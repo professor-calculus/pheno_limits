44c5282c308310ac9f390ec58247c30ee352ecb4
Bjoern Penning <bpenning@users.noreply.github.com>
2016-09-07 23:00:50 +0200

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -8,5 +8,5 @@ done
 hadd -f D5b/combined_D5b_20fb.root D5b/combine_mchi*20fb.root
 hadd -f D5b/combined_D5b_300fb.root D5b/combine_mchi*300fb.root
 hadd -f D5b/combined_D5b_3000fb.root D5b/combine_mchi*3000fb.root
-rm D5b/combine_*.root D5b/ext_vbfhinv_mchi*.txt
+#rm D5b/combine_*.root D5b/ext_vbfhinv_mchi*.txt
 python plot4EFTs.py D5b/combined_D5b_3000fb.root D5b/combined_D5b_300fb.root D5b/combined_D5b_20fb.root
